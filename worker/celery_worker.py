51b69031b10e288b962e67d95c6fc09475a64243
risto <risto.kowaczewski@gmail.com>
2022-05-12 10:56:58 +0200

    Import tasks worker

--- worker/celery_worker.py
+++ worker/celery_worker.py
@@ -30,26 +30,26 @@ def run_celery_import_job(self, import_config, credentials, source_id, tracardi_
         self.update_state(state="PROGRESS", meta={'current': x / 100, 'total': 100})
         time.sleep(.5)
 
-
-xxx(import_config={
-    "name": 'tesst',
-    "description": "desc",
-    "event_type": "import",
-    "module": "mod",
-    "config": {
-        "database_name": NamedEntity(id="mysql", name="mysql").dict(),
-        "table_name": NamedEntity(id="db", name="db").dict(),
-        "batch": 100
+if __name__ == "__main__":
+    xxx(import_config={
+        "name": 'tesst',
+        "description": "desc",
+        "event_type": "import",
+        "module": "mod",
+        "config": {
+            "database_name": NamedEntity(id="mysql", name="mysql").dict(),
+            "table_name": NamedEntity(id="db", name="db").dict(),
+            "batch": 100
+        },
+        "enabled": True,
+        "transitional": False
     },
-    "enabled": True,
-    "transitional": False
-},
-    credentials=MysqlConnectionConfig(
-        user='root',
-        password='root',
-        host='localhost',
-        port=3306
-    ).dict(),
-    source_id="a8698bd6-88d5-4263-80b7-193a79a5019b",
-    tracardi_api_url="http://localhost:8686/"
-)
+        credentials=MysqlConnectionConfig(
+            user='root',
+            password='root',
+            host='localhost',
+            port=3306
+        ).dict(),
+        source_id="a8698bd6-88d5-4263-80b7-193a79a5019b",
+        tracardi_api_url="http://localhost:8686/"
+    )
