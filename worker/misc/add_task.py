224e411a825bfef0daa38b991b5c56322b339413
risto <risto.kowaczewski@gmail.com>
2022-08-03 00:16:35 +0200

    Migration worker error fix.
    requests.exceptions.SSLError: HTTPSConnectionPool(host='es-cluster-es-http.elastic.svc.cluster.local', port=9200): Max retries exceeded with url: /638ee.tracardi-task/_doc (Caused by SSLError(SSLCertVerificationError(1, '[SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: self signed certificate in certificate chain (_ssl.c:1131)')))

--- worker/misc/add_task.py
+++ worker/misc/add_task.py
@@ -2,28 +2,27 @@ import requests
 from datetime import datetime
 import logging
 
-
 logger = logging.getLogger("logger")
 
 
 def add_task(elastic_host: str, task_index: str, name: str, job, params=None):
-
     if params is None:
         params = {}
 
     with requests.post(
-        url=f"{elastic_host}/{task_index}/_doc",
-        json={
-            "id": job.request.id,
-            "name": name,
-            "timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S"),
-            "status": "PROGRESS",
-            "type": "upgrade",
-            "progress": 0.,
-            "event_type": "missing",
-            "params": params,
-            "task_id": job.request.id
-        }
+            url=f"{elastic_host}/{task_index}/_doc",
+            json={
+                "id": job.request.id,
+                "name": name,
+                "timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S"),
+                "status": "PROGRESS",
+                "type": "upgrade",
+                "progress": 0.,
+                "event_type": "missing",
+                "params": params,
+                "task_id": job.request.id
+            },
+            verify=False
     ) as response:
 
         if response.status_code // 100 == 2:
@@ -31,4 +30,3 @@ def add_task(elastic_host: str, task_index: str, name: str, job, params=None):
 
         else:
             logger.info(msg=f"Could not add task with ID {job.request.id} due to an error: {response.text}")
-
