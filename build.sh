3cd4c2ea0480748a4688f9c8760cff66e1482954
risto <risto.kowaczewski@gmail.com>
2022-09-28 11:46:36 +0200

    Change of version to 0.7.3-dev

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build .  --no-cache -t tracardi/worker:0.7.2-dev
-docker push tracardi/worker:0.7.2-dev
+docker build .  --no-cache -t tracardi/worker:0.7.3-dev
+docker push tracardi/worker:0.7.3-dev
