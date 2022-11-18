50f127c58a40030bf3bcd938a8c362bb3a55bbb0
risto <risto.kowaczewski@gmail.com>
2022-11-18 16:30:52 +0100

    Version 0.7.3

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build .  --no-cache -t tracardi/worker:0.7.3-dev
-docker push tracardi/worker:0.7.3-dev
+docker build .  --no-cache -t tracardi/worker:0.7.3
+docker push tracardi/worker:0.7.3
