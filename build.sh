b718a4b76a3c2249433397661870898bb1ad688d
risto <risto.kowaczewski@gmail.com>
2022-12-14 18:47:55 +0100

    Version 0.7.4

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build .  --no-cache -t tracardi/worker:0.7.4-dev
-docker push tracardi/worker:0.7.4-dev
+docker build .  --no-cache -t tracardi/worker:0.7.4
+docker push tracardi/worker:0.7.4
